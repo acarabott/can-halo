aa54641c39621bebd04f746b57a3cc75c107c8df
Arthur Carabott <arthur@arthurcarabott.com>
2021-12-28 14:45:58 -0800

    build

--- src/halo.ts
+++ src/halo.ts
@@ -39,11 +39,18 @@ function createHalo(output: HTMLInputElement) {
   wrapper.appendChild(halo);
   halo.className = "halo";
 
-  const pieceLabels = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ".", "-"];
+  const pieceLabels = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ".", "-", "X"] as const;
   pieceLabels.forEach((label, i, arr) => {
     const piece = document.createElement("div");
     halo.appendChild(piece);
+    
     piece.classList.add("button", "halo-piece");
+    if (label === "X") {
+      piece.classList.add("delete");
+    } else if (label === "." || label === "-") {
+      piece.classList.add("mod");
+    }
+
     const sizePx = wrapper.clientWidth * 0.2;
     piece.style.width = `${sizePx}px`;
     piece.style.height = `${sizePx}px`;
@@ -69,6 +76,8 @@ function createHalo(output: HTMLInputElement) {
           ? output.value.includes(".")
             ? output.value
             : output.value + label
+          : label === "X"
+          ? output.value.slice(0, -1)
           : output.value + label;
     });
     return piece;
